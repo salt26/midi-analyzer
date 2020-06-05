678b77ac9b9a245a2381634dd53a4b57cefe5b92
Dantae An <48adt@naver.com>
2020-06-05 19:05:01 +0900

    initial commit

---
+++ MidiAnalyzer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Midi;
+
+namespace MidiAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = @".\..\..\midi";
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+
+                foreach (var file in di.GetFiles())
+                {
+                    if (file.Extension.ToLowerInvariant() == ".mid" ||
+                        file.Extension.ToLowerInvariant() == ".midi")
+                    {
+                        Console.WriteLine(file.Name);
+                        bool strictMode = true;
+                        MidiFile mf = new MidiFile(file.FullName, strictMode);
+
+                        Console.WriteLine("Format {0}, Tracks {1}, Delta Ticks Per Quarter Note {2}",
+                            mf.FileFormat, mf.Tracks, mf.DeltaTicksPerQuarterNote);
+                    }
+                }
+            }
+        }
+    }
+}
